6a0abd5332d506e2e2e171a6b28efb3a8f055dad
rehanrawat <88162530+rehanrawat@users.noreply.github.com>
2023-11-20 01:02:24 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -93,7 +93,7 @@ TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
 
-SCRAPEOPS_API_KEY = '3aa05ce6-c7ae-4197-8911-f39e5825a00a'
+SCRAPEOPS_API_KEY = '*******'
 
 SCRAPEOPS_PROXY_ENABLED = True
 
